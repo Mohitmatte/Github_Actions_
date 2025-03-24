89e5838e3d4b6b3ce767c839c457cceb2a9d0b99
Mohit_Matte <mattemohit2002@gmail.com>
2025-03-24 23:42:29 +0530

    updated_yaml_file

---
+++ unittesting.py
@@ -0,0 +1,11 @@
+import unittest
+from addition import add_numbers  # Import function from your main code file
+
+class TestAddition(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
